efd76956946e0de03c9be73eb081686c3c8b348d
Joseph Burnett <josephburnett79@gmail.com>
2017-11-28 21:07:09 -0800

    Save message as a Laverna note.

--- elephantine.js
+++ elephantine.js
@@ -1,8 +1,14 @@
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+var Dropbox = require('dropbox');
+
 const simpleParser = require('mailparser').simpleParser;
 
-var bucketName = 'elephantine.cc';
+var bucketName = process.env.BUCKET_NAME;
+var keyPrefix = process.env.KEY_PREFIX;
+var dropboxAccessToken = process.env.DROPBOX_ACCESS_TOKEN;
+
+var s3 = new AWS.S3();
+var dbx = new Dropbox({ accessToken: dropboxAccessToken });
 
 exports.handler = function(event, context, callback) {
     console.log('Process email');
@@ -13,7 +19,7 @@ exports.handler = function(event, context, callback) {
     // Retrieve the email from your bucket
     s3.getObject({
 	Bucket: bucketName,
-	Key: "self/" + sesNotification.mail.messageId
+	Key: keyPrefix + sesNotification.mail.messageId
     }, function(err, data) {
 	if (err) {
 	    console.log(err, err.stack);
@@ -24,6 +30,32 @@ exports.handler = function(event, context, callback) {
 		.then(mail => {
 		    console.log("Subject: " + mail.subject);
 		    console.log("Plain text:\n" + mail.text);
+		    var now = new Date().getTime();
+		    note = {
+			"type": "notes",
+			"id": sesNotification.mail.messageId,
+			"title": mail.subject,
+			"content": mail.text,
+			"taskAll": 0,
+			"taskCompleted": 0,
+			"created": now,
+			"updated": now,
+			"notebookId": "0",
+			"tags": [],
+			"isFavorite": 0,
+			"trash": 0,
+			"files": []}
+		    dbx.filesUpload(
+			{
+			    path: '/notes-db/notes/' + sesNotification.mail.messageId + '.json',
+			    contents: JSON.stringify(note)
+			})
+		        .then(function(response) {
+			    console.log(response);
+			})
+		        .catch(function(error) {
+			    console.error(error);
+			});
 		})
 		.catch(err => {
 		    console.log(err);
